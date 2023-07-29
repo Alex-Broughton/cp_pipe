Fixed inverse sign error on preKernel

python/lsst/cp/pipe/makeBrighterFatterKernel.py
@@ -339,7 +339,7 @@ class BrighterFatterKernelSolveTask(pipeBase.PipelineTask):
                 #preKernel = np.pad(self._tileArray(-1.0 * self.config.scaleFactor * np.array(inputPtc.aMatrix[ampName])), ((1, 1)))
                 
                 # Use the analytical A matrix from sampled covariance model
-                preKernel = np.pad(self._tileArray(A), ((1, 1)))
+                preKernel = np.pad(-1.0 * self._tileArray(A), ((1, 1)))
                 
             elif self.config.correlationQuadraticFit:
                 # Use a quadratic fit to the correlations as a